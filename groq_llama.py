2455f4ef181451328bc9d1fac98bad0b40da7b99
Kushagra Gupta <kushagragupta@Kushagras-MacBook-Air.local>
2025-11-27 09:56:54 +0530

    new changes

--- groq_llama.py
+++ groq_llama.py
@@ -1,17 +1,18 @@
 import os
 from dotenv import load_dotenv
 from groq import Groq
-from reAct_prompt.reAct import llama_reAct
-
+from reAct_prompt.reAct import llama_reAct, llama_react_question
+import json 
 load_dotenv(dotenv_path="env/.env")
 
 client = Groq(api_key=os.getenv("groq_llama_key"))
 
-query = "Give me the roadmap for preparing for the GATE 2026 DA exam."
-syllabus = "Probability and Statistics: Counting (permutation and combinations), probability axioms,Sample space, events, independent events, mutually exclusive events, marginal,conditional and joint probability, Bayes Theorem, conditional expectation and variance,mean, median, mode and standard deviation, correlation, and covariance, randomvariables, discrete random variables and probability mass functions, uniform, Bernoulli,binomial distribution, Continuous random variables and probability distribution function,uniform, exponential, Poisson, normal, standard normal, t-distribution, chi-squareddistributions, cumulative distribution function, Conditional PDF, Central limit theorem,confidence interval, z-test, t-test, chi-squared test."
+# query = "Give me the roadmap for preparing for the GATE 2026 DA exam."
+# syllabus = "Probability and Statistics: Counting (permutation and combinations), probability axioms,Sample space, events, independent events, mutually exclusive events, marginal,conditional and joint probability, Bayes Theorem, conditional expectation and variance,mean, median, mode and standard deviation, correlation, and covariance, randomvariables, discrete random variables and probability mass functions, uniform, Bernoulli,binomial distribution, Continuous random variables and probability distribution function,uniform, exponential, Poisson, normal, standard normal, t-distribution, chi-squareddistributions, cumulative distribution function, Conditional PDF, Central limit theorem,confidence interval, z-test, t-test, chi-squared test."
 
-def model_call():
+def model_call(query, syllabus):
 
+    query = "roadmap for" + query
     completion = client.chat.completions.create(
         model="meta-llama/llama-4-maverick-17b-128e-instruct",
         messages=[
@@ -56,3 +57,36 @@ def model_call():
 
     return data
 # model_call()
+
+
+def model_call_question(query, topic, severity):
+
+    completion = client.chat.completions.create(
+            model="meta-llama/llama-4-maverick-17b-128e-instruct",
+            messages=[
+                # {
+                #     "role": "system",
+                #     "content": llama_reAct()
+                # },
+                {
+                    "role": "system",
+                    "content": llama_react_question(query, topic, severity)
+                }
+            ]
+        )
+        # print(completion.choices[0].message.content)
+    raw_output = completion.choices[0].message.content
+
+    print(f"questions list : {raw_output}")
+    data = {}
+    if "Final Answer:" in raw_output:
+        json_index = raw_output.index("Final Answer:")
+        json_code = raw_output[json_index:]
+        json_str = json_code.split("Final Answer:")[1].strip()
+        data = json.loads(json_str)
+        return data
+        print(data)
+    else:
+        print("No json result found")
+    return data
+
